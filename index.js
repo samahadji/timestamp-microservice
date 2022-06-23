562ad395ff047511366017aa80285e84bd3a1aba
Samson Ahadji <samahadji@gmail.com>
2022-06-23 23:56:51 +0000

    Added timestamp handler

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // index.js
 // where your node app starts
-
+require('dotenv').config();
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+
+const moment = require('moment'); // for DateTime management
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,8 +23,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let m = moment.utc(req.params.date);
+  if (m.isValid()) {
+    res.json({"unix": req.params.date, "utc": m.format("ddd, DD MMM YYYY hh:mm:ss")+ " GMT"});
+  } else {
+     res.json({"error": "Invalid Date"});
+  }
 });
 
 
